f133222d958ecb68d70a47025ccb0f485e5c05b0
Graham Lee <wgin.lee@gmail.com>
2016-11-09 23:23:44 +0000

    login and logoff, cookie stuff

--- express_server.js
+++ express_server.js
@@ -1,11 +1,13 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
-
-app.set("view engine", "ejs")
-
+const express = require("express");
+const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 8080; // default port 8080
+
+const app = express();
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,7 +27,10 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -34,7 +39,10 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id ,urls: urlDatabase};
+  let templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.id ,urls: urlDatabase
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -60,6 +68,17 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   console.log(req.params.shortURL);  // debug statement to see POST parameters
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");         // Respond with 'Ok' (we will replace this)
+});
+
+app.post("/login", (req, res) => {
+  //set cookie param 'username' to value submitted
+  res.cookie('username', req.body.username);
+  res.redirect("/");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
